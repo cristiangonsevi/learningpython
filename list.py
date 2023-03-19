5a33c2081a059f13d6d49e6084cd18198de70025
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-03-18 18:28:23 -0600

    feat: :sparkles: list in python learned list methods

---
+++ list.py
@@ -0,0 +1,13 @@
+numbers = [1,2,3,4,5,6]
+
+# insert into list at index 
+numbers.insert(0, -1)
+
+# append number to list
+numbers.append(7)
+
+# remove value from list
+numbers.remove(1)
+
+# clear full list 
+numbers.clear()
